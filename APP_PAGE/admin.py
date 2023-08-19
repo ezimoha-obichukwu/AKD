497d3f297eb934a6dd9d4e4f10c98a2a7f382f24
ezimoha-obichukwu <77929524+ezimoha-obichukwu@users.noreply.github.com>
2023-08-19 02:53:33 +0100

    'second'

--- APP_PAGE/admin.py
+++ APP_PAGE/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import User, Prize, Spin
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "email", "points", "spins_left"]
+    list_filter = ["username", "email"]
+    search_fields = ["username", "email"]
+
+@admin.register(Prize)
+class PrizeAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+    search_fields = ["name"]
+
+@admin.register(Spin)
+class SpinAdmin(admin.ModelAdmin):
+    list_display = ["user", "prize", "timestamp", "is_winner"]
+    search_fields = ["user"]
--- APP_PAGE/models.py
+++ APP_PAGE/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    points = models.IntegerField(default=0)
+    spins_left = models.IntegerField(default=3)  # Number of spins user has left
+
+    def __str__(self):
+        return self.username
+
+class Prize(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='prize_images/')
+    quantity = models.IntegerField(default=0)  # Number of available prizes
+
+    def __str__(self):
+        return self.name
+
+class Spin(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    prize = models.ForeignKey(Prize, on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_winner = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.prize.name} - Winner: {self.is_winner}"
